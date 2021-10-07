539e7d9e99d2684b55048737c5c2bfb4038a1f08
Leonardo <leo.compiler@gmail.com>
2021-10-07 09:27:41 -0300

    included screen home by list/tableview components created

---
+++ TesteViewCode/View/HomeItem.swift
@@ -0,0 +1,20 @@
+//
+//  HomeItemViewController.swift
+//  TesteViewCode
+//
+//  Created by LEONARDO A SILVEIRA on 06/10/21.
+//
+
+import Foundation
+import UIKit
+
+final class HomeItem : UITableViewCell {
+    
+    var title : UILabel = { return UILabel()}()
+    var subTitle : UILabel = { return UILabel()}()
+    var iconImage : UIImage = { return UIImage()}()
+    
+    func setUpCell() {
+        
+    }
+}
--- TesteViewCode/View/HomeViewController.swift
+++ TesteViewCode/View/HomeViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 final class HomeViewController : UIViewController {
     
+     
+    
     private let model : Model?;
-    public lazy var titleScreen: UILabel = {
+    
+    lazy var titleScreen: UILabel = {
         let titleScreen = UILabel( frame: .zero)
         titleScreen.translatesAutoresizingMaskIntoConstraints = false
         return titleScreen
+    }()
+    lazy var tableView : UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+        
     }()
     
     init( route: Coordinator) {
@@ -30,4 +39,41 @@ final class HomeViewController : UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray2
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+}
+
+
+
+extension HomeViewController: EmployeeViewModelDelegate {
+    func reloadUI() {
+        tableView.reloadData()
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows()
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = EmployeeCell()
+        cell.nameLabel.text = viewModel.name(at: indexPath.row)
+        cell.cargoLabel.text = viewModel.cargo(at: indexPath.row)
+        cell.photo.image = UIImage(named: viewModel.foto(at: indexPath.row))
+        cell.setUpCell()
+
+        return cell
+    }
+
+
 }
